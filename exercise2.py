9cc84e6ce76f7920b136a08e5665532ff343e769
hirangi <hira13@gmail.com>
2015-01-26 11:42:32 +0000

    Chaning the sequence and adding the print message

--- exercise2.py
+++ exercise2.py
@@ -4,3 +4,13 @@ from __future__ import division
 
 sequence = "ACTGATCGATTACGTATAGTATTTGCTATCATACATATATATCGATGCGTTCAT"
 
+sequence2 = sequence.replace("A", "t")
+
+seqeunce3 = sequence2.replace("C", "g")
+
+sequence4 = seqeunce3.replace("G", "c")
+
+sequence5 = sequence4.replace("T", "a")
+
+
+print(sequence5.upper())
