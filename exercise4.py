35fca10e04d2ffb9b2621e270415515e768515c4
hirangi <hira13@gmail.com>
2015-01-26 11:50:12 +0000

    added the seond part and printed what percent of the sequence is coding

--- exercise4.py
+++ exercise4.py
@@ -11,3 +11,13 @@ exon_2 = sequence[91:len(sequence)]
 
 print("Coding region: " + exon_1 + exon_2)
 
+
+#2
+
+lengthtotal = len(sequence)
+
+lengthexon = len(exon_1) + len (exon_2)
+
+percent = lengthexon/lengthtotal * 100
+
+print("The percentage of the sequence that is coding is: " + str(percent) + "%")
