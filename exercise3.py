a535228f68c120b304032e4b7b1e255fbad4cc7e
hirangi <hira13@gmail.com>
2015-01-26 11:47:55 +0000

    Second part and printed both of the strands lenghts

--- exercise3.py
+++ exercise3.py
@@ -4,3 +4,11 @@ from __future__ import division
 
 sequence = "ACTGATCGATTACGTATAGTAGAATTCTATCATACATATATATCGATGCGTTCAT"
 
+strand1 = sequence.find("GAATTC") + 1
+
+strand2 = len(sequence) - strand1
+
+print("The first strand is of length:" + str(strand1))
+
+
+print("The second strand is of length:" + str(strand2))
